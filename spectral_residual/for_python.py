4bd922a50fda6d6123e4f88632945222b347ff78
hiroaka0208 <u957219i@fbs.osaka-u.ac.jp>
2021-06-28 17:14:54 +0900

    OpenCVのspectral residual saliency modelを再現

--- spectral_residual/for_python.py
+++ spectral_residual/for_python.py
@@ -3,7 +3,7 @@ import numpy as np
 from matplotlib import pyplot as plt
 
 
-def SpectraResidualSaliency(image):
+def spectral_residual_saliency(image):
     h, w, c = image.shape
     output_saliency = np.zeros((64,64,3))
 
@@ -13,19 +13,21 @@ def SpectraResidualSaliency(image):
 
         fft = cv2.dft(np.float32(img), flags = cv2.DFT_COMPLEX_OUTPUT)
         amp, phase = cv2.cartToPolar(fft[:,:,0],fft[:,:,1])
-        amp = np.exp(np.log(amp) - cv2.boxFilter(np.log(amp), -1, (3,3),cv2.BORDER_DEFAULT))
+        SR = np.exp(np.log(amp) - cv2.boxFilter(np.log(amp), -1, (3,3),cv2.BORDER_DEFAULT))
 
-        fft[:,:,0], fft[:,:,1] = cv2.polarToCart(amp,phase)
-        ifft = cv2.dft(fft, flags = (cv2.DFT_INVERSE | cv2.DFT_SCALE))
+        fft[:,:,0], fft[:,:,1] = cv2.polarToCart(SR,phase)
+        ifft = cv2.dft(fft, flags = cv2.DFT_INVERSE)
         amp, phase = cv2.cartToPolar(ifft[:,:,0], ifft[:,:,1])
-        amp = amp**2
+        # amp = amp*amp
 
-        amp = cv2.GaussianBlur(amp,(3,3),8,0,cv2.BORDER_DEFAULT)
-        amp = cv2.normalize(amp, 0., 1., cv2.NORM_MINMAX)
+        amp = cv2.GaussianBlur(amp,(5,5),8,0,cv2.BORDER_DEFAULT)
+        amp = amp*amp
+        min_val, max_val, min_idx, max_idx = cv2.minMaxLoc(amp) 
+        amp = amp/max_val
         output_saliency[:,:,i] = amp
 
     saliencyMap = 0.333*output_saliency[:,:,0] + 0.333*output_saliency[:,:,1] + 0.333*output_saliency[:,:,2]
     saliencyMap = cv2.resize(saliencyMap, (h,w), cv2.INTER_LINEAR_EXACT)
-    saliencyMap = (saliencyMap * 255).astype("uint8")
+    saliencyMap = (saliencyMap * 255).astype("uint8") 
 
     return saliencyMap
